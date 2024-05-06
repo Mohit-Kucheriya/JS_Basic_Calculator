cc5fb7e70a429014804a28a46093a2225d593c5a
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-05-06 19:48:40 +0530

    Basic Calculator

---
+++ app.js
@@ -0,0 +1,18 @@
+const inp = document.getElementById("cal-number");
+const result = document.getElementById("result");
+
+function clickBtn(el) {
+    console.log(el.innerText);
+    const exp = `${inp.value}${el.innerText}`;
+    inp.value = exp;
+    if (['+', '-', '*', '/','='].includes(el.innerText)) return; {
+        result.value = eval(exp);
+        //here we have used to evalute the expression
+    }
+
+}
+
+function reset() {
+    inp.value = "";
+    result.value = "";
+}
