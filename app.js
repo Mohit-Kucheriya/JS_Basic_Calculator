b3bfa4cbbc8fdf6f53ccd56f514c2b3fe78e32fd
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-05-07 10:21:16 +0530

    Done

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ function clickBtn(el) {
     console.log(el.innerText);
     const exp = `${inp.value}${el.innerText}`;
     inp.value = exp;
-    if (['+', '-', '*', '/','='].includes(el.innerText)) return; {
+    if (['+', '-', '*', '/', '='].includes(el.innerText)) return; {
         result.value = eval(exp);
-        //here we have used to evalute the expression
+        //here we have used eval() to evalute the expression
     }
 
 }
